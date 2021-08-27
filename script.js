7fb63c27d640b8a26df042fcfd868e0b582cecda
MacielDouglas <douglasmaciel.dm@gmail.com>
2021-08-27 17:33:31 -0300

    adicionando cor cobra azul

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function criarBG() {
 
 function criarCobra() {
     for (i = 0; i < snake.length; i++) {
-        context.fillStyle = "green";
+        context.fillStyle = "blue";
         context.fillRect(snake[i].x, snake[i].y, box, box);
         
     }
